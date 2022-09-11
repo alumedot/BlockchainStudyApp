a40a5dc09b591e269d0d56c55eccefe3ebe0309d
Alexey Kozlov <alumedot@icloud.com>
2022-09-11 18:44:03 +0100

    feat: run the app, is_valid route

--- blockchain.py
+++ blockchain.py
@@ -98,3 +98,22 @@ def get_chain():
         'length': len(blockchain.chain)
     }
     return jsonify(response), 200
+
+
+# Check if the Blockchain is valid
+@app.route('/is_valid', methods=['GET'])
+def is_valid():
+    is_valid = blockchain.is_chain_valid(blockchain.chain)
+    if is_valid:
+        response = {
+            'message': 'All good, the blockchain is valid'
+        }
+    else:
+        response = {
+            'message': 'The blockchain is not valid!'
+        }
+    return jsonify(response), 200
+
+
+# Running the app
+app.run(host='0.0.0.0', port=4000)
