89dc6ac1859a933c3d7dbf6dde47cd905a5c1878
Alexey Kozlov <alumedot@icloud.com>
2022-09-11 13:48:18 +0100

    feat: get_chain route

--- blockchain.py
+++ blockchain.py
@@ -71,6 +71,7 @@ app = Flask(__name__)
 # Creating a Blockchain
 blockchain = Blockchain()
 
+
 # Mining a new block
 @app.route('/mine_block', methods=['GET'])
 def mine_block():
@@ -85,5 +86,15 @@ def mine_block():
         'timestamp': block['timestamp'],
         'proof': block['proof'],
         'previous_hash': block['previous_hash']
+    }
+    return jsonify(response), 200
+
+
+# Getting the full Blockchain
+@app.route('/get_chain', methods=['GET'])
+def get_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain)
     }
     return jsonify(response), 200
