f850841862ba3ff88c7f2c475d81c5e5cdcf1387
Alexey Kozlov <alumedot@icloud.com>
2022-11-15 18:49:37 +0300

    feat: /add_transaction post function

--- dotCoin.py
+++ dotCoin.py
@@ -8,6 +8,7 @@ import requests
 from uuid import uuid4
 from urllib.parse import urlparse
 
+
 # Part 1 - Building a Blockchain
 
 class Blockchain:
@@ -69,7 +70,7 @@ class Blockchain:
             block_index += 1
         return True
 
-    def add_transactions(self, sender, receiver, amount):
+    def add_transaction(self, sender, receiver, amount):
         self.transactions.append({
             'sender': sender,
             'receiver': receiver,
@@ -101,10 +102,14 @@ class Blockchain:
             return True
         return False
 
+
 # Part 2 - Mining our Blockchain
 
 # Creating a Web App
 app = Flask(__name__)
+
+# Creating an address for the node on Port 5000
+node_address = str(uuid4()).replace('-', '')
 
 # Creating a Blockchain
 blockchain = Blockchain()
@@ -117,13 +122,15 @@ def mine_block():
     previous_proof = previous_block['proof']
     proof = blockchain.proof_of_work(previous_proof)
     previous_hash = blockchain.hash(previous_block)
+    blockchain.add_transaction(sender=node_address, receiver='Alex', amount=1)
     block = blockchain.create_block(proof, previous_hash)
     response = {
         'message': 'Congratulations, you just mined a block!',
         'index': block['index'],
         'timestamp': block['timestamp'],
         'proof': block['proof'],
-        'previous_hash': block['previous_hash']
+        'previous_hash': block['previous_hash'],
+        'transactions': block['transactions']
     }
     return jsonify(response), 200
 
@@ -151,6 +158,21 @@ def is_valid():
             'message': 'The blockchain is not valid!'
         }
     return jsonify(response), 200
+
+
+# Adding a new transaction to the Blockchain
+@app.route('/add_transaction', methods=['POST'])
+def add_transaction():
+    json = request.get_json()
+    transaction_keys = ['sender', 'receiver', 'amount']
+
+    if not all(key in json for key in transaction_keys):
+        return 'Some elements of the transaction are missing', 400
+
+    index = blockchain.add_transaction(json['sender'], json['receiver'], json['amount'])
+    response = {'message': f'This transaction will be added to block {index}'}
+
+    return jsonify(response), 201
 
 # Part 3 - Decentralizing our blockchain
 
